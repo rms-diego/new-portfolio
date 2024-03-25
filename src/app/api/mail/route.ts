8a14cd25b945f88ea0c3086751f3f293d693c793
Diego Ramos <diegorbrito9@gmail.com>
2024-03-25 10:45:17 -0300

    feat: criando rota para envio de email

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,8 +1,23 @@
-import { sendMail } from '@/service/sendMail';
+import { Resend } from 'resend';
+
 export async function POST(request: Request) {
   const { name, email, emailTitle, emailContent } = await request.json();
 
-  await sendMail({ name, email, emailTitle, emailContent });
+  const { RESEND_API_KEY, EMAIL } = process.env;
+  const resend = new Resend(RESEND_API_KEY!);
+
+  const { error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: [EMAIL!],
+    subject: `Portfolio: ${name} - ${emailTitle}`,
+    text: `${emailContent}\n\n Enviado por: ${email}`,
+  });
+
+  if (error) {
+    const errorMessage = JSON.stringify(error);
+
+    return new Response(errorMessage, { status: 400 });
+  }
 
   return new Response(null, { status: 204 });
 }
