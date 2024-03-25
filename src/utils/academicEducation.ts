a77d761a61441d85f85c8444f55d9a950ecda800
Diego Ramos <diegorbrito9@gmail.com>
2024-03-24 23:02:55 -0300

    feat: criando arquivos para experiencia e educação

---
+++ src/utils/academicEducation.ts
@@ -0,0 +1,37 @@
+export const courses = [
+  {
+    title: 'Ciência da computação - Estácio',
+    duration: '2024 - em curso',
+    description:
+      'O curso de Ciência da Computação oferece uma base sólida em programação, algoritmos, inteligência artificial e outras áreas-chave da tecnologia. Os alunos desenvolvem habilidades analíticas e de resolução de problemas, preparando-os para carreiras em desenvolvimento de software, análise de dados, cibersegurança e empreendedorismo tecnológico.',
+  },
+
+  {
+    title: 'Bootcamp - Rocketseat',
+    duration: '2022 - 2023',
+    description:
+      'A Rocketseat é plataforma brasileira de educação em tecnologia, conhecida por cursos online práticos e dinâmicos em programação, desenvolvimento web e mobile. A plataforma oferece diversos cursos nomeados como "trilhas", voltadas tanto para o desenvolvimento frontend quanto para o desenvolvimento backend.',
+  },
+
+  {
+    title: 'Desenvolvimento Web - Trybe',
+    duration: '2021 - 2023',
+    description: `A Trybe é uma escola de desenvolvimento web que tem comprometimento genuíno com sucesso profissional das pessoas estudantes. Com o Modelo de Sucesso Compartilhado, quem estuda na Trybe tem a opção de pagar apenas quando já estiver trabalhando.
+
+    São mais de 1500 horas de formação que aborda fundamentos de desenvolvimento web, desenvolvimento front-end, desenvolvimento Back-end, conceitos de ciência da computação, metodologias ágeis e habilidades comportamentais.`,
+  },
+
+  {
+    title: 'Bootcamp - Alura',
+    duration: '2020 - 2021',
+    description:
+      'A Alura é uma plataforma de educação online reconhecida, oferecendo cursos em programação, desenvolvimento web, data science e marketing digital. Com abordagem prática e ministrados por especialistas, os cursos incluem vídeo-aulas, exercícios e projetos reais. Como aluno da Alura, aprimorei habilidades técnicas e profissionais, preparando-me para desafios profissionais. A plataforma destaca-se pela qualidade e compromisso com o sucesso dos alunos.',
+  },
+
+  {
+    title: 'Desenvolvimento de sistemas - Senai Cimatec',
+    duration: '2020 - 2021 (incompleto)',
+    description:
+      'O curso técnico em Desenvolvimento de Sistemas oferece uma formação prática e abrangente em programação, desenvolvimento web, banco de dados e segurança da informação, preparando os alunos para carreiras como desenvolvedores de software. Com duração de aproximadamente 3 anos, os alunos adquirem habilidades essenciais por meio de aulas teóricas, laboratórios práticos e projetos reais, recebendo ao final um certificado reconhecido pelo Ministério da Educação.',
+  },
+];
---
+++ src/utils/professionalExperience.ts
@@ -0,0 +1,56 @@
+export const professionalExperience = [
+  {
+    title: 'Desenvolvedor fullstack - Arrow Digital - 51.190.073/0001-24',
+    duration: '11/2023 - Atualmente',
+    description: `
+      Criação de REST APIS usando express, typescript, mongoose, criação de testes unitários e testes de integração, integração direta com APIS de terceiros e web services, como por exemplo, stripe, AWS S3, firebase, openAI. 
+
+      Criação de scripts para automatização de processos manuais, como dump do banco banco de dados dentre outros.
+    `,
+
+    technologies: [
+      'Express',
+      'Typescript',
+      'Amazon Web Services (s3, ec2)',
+      'Mongo db',
+      'Mongoose',
+      'Firebase',
+      'Linux',
+      'Vitest',
+    ],
+  },
+  {
+    title: 'Desenvolvedor fullstack - Freelancer',
+    duration: '01/2022 - Atualmente',
+    description:
+      'Construção de aplicativos web e mobile sob demanda, atuo como fullstack, com bastante foco em segurança, performance e escalabilidade.',
+
+    technologies: [
+      'React.js | Next.js',
+      'React Native com expo',
+      'Nodejs | Fastify | Express | nest.js',
+      'Mysql | Postgres | Sqlite',
+      'Mongodb',
+      'Redis',
+      'Typescript',
+      'Docker',
+    ],
+  },
+  {
+    title: 'Desenvolvedor fullstack - Resolva Meu Look - 37.539.385/0001-70',
+    duration: '08/2022 - 01/2022',
+    description:
+      'Construção de plataforma de para gerenciamento de "looks". Trabalhava fullstack no frontend na criação de paginas web com reactjs e no backend com nodejs.',
+
+    technologies: [
+      'React.js',
+      'Nodejs',
+      'MySQL',
+      'Mongodb',
+      'Redis',
+      'Amazon Web Services ( s3, rds, ec2 )',
+      'Typescript',
+      'Docker',
+    ],
+  },
+];
